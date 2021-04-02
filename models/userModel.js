1669ede9c668dc3f99cbae05d95393f0cb3323de
GAURAV <gauravpatil8433@gmail.com>
2021-04-02 14:57:22 +0530

    events

--- models/userModel.js
+++ models/userModel.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     email: {type: String,required:true},
+    first_name: {type: String,required:true},
+    last_name: {type: String,required:true},
+    classs: {type: String,required:true},
     passwordHash: {type: String, required: true},
 });
 
